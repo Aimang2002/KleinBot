60125c915850695a948ef7555d5627c9a9d052c1
Aimang2002 <1472807646@qq.com>
2026-06-04 21:52:50 +0800

    refactor: 将构造改成注入构造，禁止Dock类的实例

--- src/Command/GeneratePictureCommand.cpp
+++ src/Command/GeneratePictureCommand.cpp
@@ -1,7 +1,6 @@
 #include "GeneratePictureCommand.h"
 #include "../utils/Utils.hpp"
 #include "../ConfigManager/ConfigManager.h"
-#include "../ModelApiCaller/Dock.hpp"
 
 bool GeneratePictureCommand::canHandle(const std::string &message)
 {
@@ -14,26 +13,28 @@ bool GeneratePictureCommand::canHandle(const std::string &message)
 
 CommandResult GeneratePictureCommand::execute(const CommandContext &ctx)
 {
-    std::string prompt = utils::CP_split(ctx.data.raw_message, "#生成图片");
+    std::string prompt = utils::CP_split(ctx.data.plain_text, "#生成图片");
     if (prompt.empty())
     {
-        prompt = utils::CP_split(ctx.data.raw_message, "#图片生成");
+        prompt = utils::CP_split(ctx.data.plain_text, "#图片生成");
     }
-    std::unique_ptr<Dock> dock = std::make_unique<Dock>();
     if (prompt.empty())
     {
         return {"提示词为空！"};
     }
-    std::string endpoint = ConfigManager::getInstance().configVariable("DRAW_MODEL_ENDPOINT");
-    std::string api_key = ConfigManager::getInstance().configVariable("DRAW_MODEL_API_KEY");
-    std::string model = ConfigManager::getInstance().configVariable("DRAW_MODEL");
-    auto response = dock->RequestDraw(endpoint, api_key, model, prompt);
+
+    ChatModel model;
+    model.endpoint = ConfigManager::getInstance().configVariable("DRAW_MODEL_ENDPOINT");
+    model.api_key = ConfigManager::getInstance().configVariable("DRAW_MODEL_API_KEY");
+    model.api_standard = ConfigManager::getInstance().configVariable("DRAW_MODEL_APISTANDARD");
+    std::string modelName = ConfigManager::getInstance().configVariable("DRAW_MODEL");
+    auto response = dock.RequestDraw(model, modelName, prompt);
 
     if (response.code >= 400)
     {
         return {"网络异常！"};
     }
-    std::string base64 = response.data_base64;
+    std::string base64 = response.image_base64;
     base64 = base64.insert(0, "base64://");
     std::string result = utils::CQCode("image", "file", base64);
     return {result, MessageType::CQ};
--- src/Command/GeneratePictureCommand.h
+++ src/Command/GeneratePictureCommand.h
@@ -6,13 +6,18 @@
  */
 #include <iostream>
 #include "Command.h"
+#include "../ModelApiCaller/Dock.hpp"
 
 class GeneratePictureCommand : public Command
 {
 public:
+    GeneratePictureCommand(Dock &dock) : dock(dock) {}
     bool canHandle(const std::string &message) override;
     CommandResult execute(const CommandContext &ctx) override;
     std::string help() const override { return "根据提示词，使用大模型生成图片。"; }
+
+private:
+    Dock &dock;
 };
 
 #endif // GENERATEPICTURECOMMAND_H
